2274272981aaae670dc7be2aea1beaaf8a982cf4
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-12 11:52:07 -0500

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Danielle Lewis on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
